9aca073f97ea1838bdf7f16a8af614d9221d4b45
Yağız Zengin <yagizzengin73@gmail.com>
2024-08-04 11:09:49 +0300

    a34x: include emulated storage

--- twrp_a34x.mk
+++ twrp_a34x.mk
@@ -5,6 +5,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
